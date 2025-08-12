1664b8455eb8552ad9d91950a753a9640f2942f2
Vishal <vkant2005@gmail.com>
2025-08-12 23:43:23 +0530

    tried solving meetingRoom prob

---
+++ greedyAlgorithm/meetingRoom.js
@@ -0,0 +1,2 @@
+// Given one meeting room and N meetings represented by two arrays, start and end, where start[i] represents the start time of the ith meeting and end[i] represents the end time of the ith meeting, determine the maximum number of meetings that can be accommodated in the meeting room if only one meeting can be held at a time.
+
