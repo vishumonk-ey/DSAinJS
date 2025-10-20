c0665bbc3f3b046cd2e329b2f88f7c1081640284
Vishal <vkant2005@gmail.com>
2025-10-20 22:37:45 +0530

    added solution

---
+++ VisaOA/climbingStaris.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const dp = new Array(50).fill(-1);
+var climbStairs = function (n) {
+  if (n == 0) {
+    return 1;
+  }
+  if (n < 0) {
+    return 0;
+  }
+  if (dp[n] != -1) {
+    return dp[n];
+  }
+  return (dp[n] = climbStairs(n - 1) + climbStairs(n - 2));
+};
