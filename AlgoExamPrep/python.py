c443f45445e213d878b9b93fef8ee9cc4cda3ddb
Vishal <vkant2005@gmail.com>
2025-12-21 23:34:44 +0530

    added sol and notes

---
+++ AlgoExamPrep/python.py
@@ -0,0 +1,10 @@
+abs() ==> for absolute value
+math.floor() 
+len() ==> to get length
+mp = {} # to create the dictionary
+min() # to find the min b/w two or more values
+min_val = float('-inf')
+max_val = float('inf') # initializing
+enumerate(arr , start = 1) # for index and val
+range(arr.len) # for i = 0 -> arrlength /(random val)
+for i in string # directly iterate over the values
---
+++ DynamicProgramming/CoinCombinations2.js
@@ -0,0 +1,28 @@
+const coins = [2, 3, 5];
+const target = 9;
+const dp = Array.from({ length: coins.length }, () =>
+  new Array(target + 1).fill(-1)
+);
+function numOfWays(coins, i, j) {
+  if (j == 0) {
+    return 1;
+  } else if (i < 0 || j < 0) {
+    return 0;
+  }
+  if (dp[i][j] == -1) {
+    // console.log("ran");
+    dp[i][j] = 0;
+    let count = 1;
+    while (count * coins[i] <= j) {
+      dp[i][j] += numOfWays(coins, i - 1, j - count * coins[i]);
+      count++;
+    }
+    dp[i][j] += numOfWays(coins, i - 1, j);
+    // dp[i][j] =
+    //   numOfWays(coins, i - 1, j - coins[i]) + numOfWays(coins, i - 1, j);
+  }
+  return dp[i][j];
+}
+console.log(dp);
+
+console.log(numOfWays(coins, coins.length - 1, target));
